17870fc05fee764fd3fb1f29baf29a53cfb61097
JaryJay <47123100+JaryJay@users.noreply.github.com>
2020-10-03 21:49:23 -0400

    Create CookieClickerApp.java

---
+++ src/main/CookieClickerApp.java
@@ -0,0 +1,17 @@
+package main;
+
+/**
+ * 
+ * Let's experiment with some general game design stuff with a simple game of
+ * cookie clicker :)
+ * 
+ * @author Jary, Donny
+ *
+ */
+public class CookieClickerApp {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
